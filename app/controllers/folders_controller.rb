b152467e8d1307a33013b4826217128397d5656a
Eric Rouach <erouach@hotmail.fr>
2018-02-03 18:07:53 +0100

    can create folders, edit and delete them

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,22 +1,50 @@
 class FoldersController < ApplicationController
+  before_action :set_folder, only: %i[show edit update destroy]
+
+  def index
+    @folders = Folder.where(user_id: current_user.id)
+  end
+
   def new
+    @folder = Folder.new
   end
 
   def create
+    @folder = Folder.new(folder_params)
+    @folder.user = current_user
+    if @folder.save!
+      redirect_to folders_path
+    else
+      redirect_to new_folder_path, alert: 'Something went wrong'
+    end
   end
 
   def show
   end
 
-  def index
-  end
-
-  def edit
-  end
+  def edit;end
 
   def update
+    @folder.update(folder_params)
+    if @folder.save
+      redirect_to folders_path
+    else
+      redirect_to edit_folder_path(folder.id), alert: 'Something went wrong'
+    end
   end
 
   def destroy
+    @folder.destroy
+    redirect_to folders_path
+  end
+
+  private
+
+    def set_folder
+    @folder = Folder.find(params[:id])
+  end
+
+  def folder_params
+    params.require(:folder).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,43 @@
 Rails.application.routes.draw do
-  get 'answer/new'
+  resources :folders
+  resources :questions do
+    resources :answers
+  end
+  # get 'answer/new'
 
-  get 'answer/create'
+  # get 'answer/create'
 
-  get 'answer/edit'
+  # get 'answer/edit'
 
-  get 'answer/update'
+  # get 'answer/update'
 
-  get 'answer/show'
+  # get 'answer/show'
 
-  get 'answer/destroy'
+  # get 'answer/destroy'
 
-  get 'question/new'
+  # get 'question/new'
 
-  get 'question/create'
+  # get 'question/create'
 
-  get 'question/edit'
+  # get 'question/edit'
 
-  get 'question/update'
+  # get 'question/update'
 
-  get 'question/destroy'
+  # get 'question/destroy'
 
-  get 'folder/new'
+  # get 'folder/new'
 
-  get 'folder/create'
+  # get 'folder/create'
 
-  get 'folder/show'
+  # get 'folder/show'
 
-  get 'folder/index'
+  # get 'folder/index'
 
-  get 'folder/edit'
+  # get 'folder/edit'
 
-  get 'folder/update'
+  # get 'folder/update'
 
-  get 'folder/destroy'
+  # get 'folder/destroy'
 
   devise_for :users
   root to: 'pages#home'
